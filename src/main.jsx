e35c54de19c4c85c1a39e9a37864f24d11f4194c
ahmedseyam01 <ahmedehabsiam@gmail.com>
2026-07-27 23:01:43 +0300

    fix: resolve node_modules import path in main.jsx for Vercel production build

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import { HeroUIProvider } from "@heroui/react";
 import CounterContextProvider from "./Context/CounterContext.jsx";
 import AuthContextProvider from "./Context/AuthContext.jsx";
